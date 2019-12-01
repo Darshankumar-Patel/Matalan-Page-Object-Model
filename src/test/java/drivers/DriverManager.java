fab87bb898240060eccd752881114de9389dea54
Darshankumar-Patel <Darshankumar-Patel>
2019-12-01 14:38:43 +0000

    Waits updated

--- src/test/java/drivers/DriverManager.java
+++ src/test/java/drivers/DriverManager.java
@@ -3,8 +3,12 @@ package drivers;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
--- src/test/java/pageElement/HomePage.java
+++ src/test/java/pageElement/HomePage.java
@@ -4,13 +4,17 @@ import drivers.DriverManager;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage extends DriverManager {
 
 public void searchItem () {
 
     driver.findElement(By.name("q")).clear();
+    new WebDriverWait(driver,10).until(ExpectedConditions.visibilityOf(driver.findElement(By.name("q"))));
     driver.findElement(By.name("q")).sendKeys("Boys school uniform");
+    new WebDriverWait(driver,10).until(ExpectedConditions.visibilityOf(driver.findElement(By.name("q"))));
     driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
     }
 }
