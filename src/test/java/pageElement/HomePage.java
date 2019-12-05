a44834bde59e6baa16037f596884089b0b3abd26
Darshankumar-Patel <Darshankumar-Patel>
2019-12-05 11:44:18 +0000

    Put Waits in place

--- src/test/java/pageElement/HomePage.java
+++ src/test/java/pageElement/HomePage.java
@@ -12,9 +12,9 @@ public class HomePage extends DriverManager {
 public void searchItem () {
 
     driver.findElement(By.name("q")).clear();
-    new WebDriverWait(driver,10).until(ExpectedConditions.visibilityOf(driver.findElement(By.name("q"))));
     driver.findElement(By.name("q")).sendKeys("Boys school uniform");
-    new WebDriverWait(driver,10).until(ExpectedConditions.visibilityOf(driver.findElement(By.name("q"))));
     driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+    new WebDriverWait(driver,30).until(ExpectedConditions.visibilityOf(driver.findElement(By.name("q"))));
+
     }
 }
