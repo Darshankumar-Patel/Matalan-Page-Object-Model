d6ee0c3ac6e691b29d364c1bb846f25e6ee5bc7d
Darshankumar-Patel <Darshankumar-Patel>
2019-12-08 14:49:33 +0000

    added search feature

--- src/test/java/utils/RunCukesTest.java
+++ src/test/java/utils/RunCukesTest.java
@@ -6,7 +6,7 @@ import gherkin.formatter.model.Feature;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/random.feature",glue = {"stepDefs"},plugin = "json:target/cucumber.json")
+@CucumberOptions(features = "src/test/resources/search.feature",glue = {"stepDefs"},plugin = "json:target/cucumber.json")
 public class RunCukesTest {
 
 }
